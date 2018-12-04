5ca2321d6debb96959a94eee957f5d999013d951
Max Hopley <maxhopley@Maxs-MacBook-Pro.local>
2018-12-03 19:36:05 -0500

    percentage change calculator and barplot vis

--- src/data/pct_change_calculator.py
+++ src/data/pct_change_calculator.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from src.visualization import visualize_model
 from src.data import twitterAnalyzer
+import matplotlib.pyplot as plt
 
 
 def pct_change(df, start, end, hours_offset):
@@ -22,11 +23,32 @@ def pct_change(df, start, end, hours_offset):
     return pct_change_df
 
 
-def barplot(sentiments, pct_changes):
-    x = sentiments.index
-    y = sentiments.iloc[:, 0]
-    z = pct_changes.iloc[:, 0]
-    #not done yet
+def barplot(sentiments, pct_changes, hours_offset):
+
+    x = pct_changes.index #times, for each pair of bars
+
+    ind = np.arange(len(x[hours_offset:])) #adjust for offset making fewer data points
+
+    sentiments = sentiments.iloc[0:ind[-1]+1, 0]/100
+    pct_changes = pct_changes.iloc[hours_offset:, 0] #offset pct changes to they come after corresponding sentiments
+    width = .3
+
+    fig, ax = plt.subplots()
+    splot = ax.bar(ind+width/2, sentiments, width, color="r", align="edge")
+
+    pct_changes = pct_changes
+    pcplot = ax.bar(ind+width/2, pct_changes, -width, color="b", align="edge")
+
+    ax.set_ylabel("must set two y labels")
+    ax.set_title("title")
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(x)
+    ax.legend((splot[0], pcplot[0]), ("Sentiment/100", "% Change"))
+    ax.autoscale_view()
+    print("The correlation is " + str(np.corrcoef(sentiments, pct_changes)[0, 1]))
+
+    plt.show()
+
 
 if __name__ == "__main__":
     ## For testing purposes
@@ -37,10 +59,9 @@ if __name__ == "__main__":
     price_df = price_df.set_index(pd.DatetimeIndex(price_df['Date']))
     price_df = price_df.drop(columns="Date")
     price_df = price_df.iloc[::-1]
-    print(price_df[:9])
+    #print(price_df[:9])
     #print(tweet_df)
     pct_change_df = pct_change(price_df, "2017-12-12 15:00:00", "2017-12-12 19:00:00", 1)
-    print(pct_change_df)
-
-
+    #print(pct_change_df)
+    barplot(tweet_df, pct_change_df, 1)
 
